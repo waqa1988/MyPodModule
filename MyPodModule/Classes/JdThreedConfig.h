45619f850532bb52fdb7620da2bebb826459600f
wangqiang146 <wangqiang18@jd.com>
2020-09-11 11:59:00 +0800

    add codes

---
+++ MyPodModule/Classes/JdThreedConfig.h
@@ -0,0 +1,19 @@
+//
+//  JdThreedConfig.h
+//  MyThreedFramework
+//
+//  Created by 王强 on 2020/9/10.
+//  Copyright © 2020 王强. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JdThreedConfig : NSObject
+
+@property(nonatomic, assign) NSString *version;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyPodModule/Classes/JdThreedConfig.m
@@ -0,0 +1,18 @@
+//
+//  JdThreedConfig.m
+//  MyThreedFramework
+//
+//  Created by 王强 on 2020/9/10.
+//  Copyright © 2020 王强. All rights reserved.
+//
+
+#import "JdThreedConfig.h"
+
+@implementation JdThreedConfig
+
+-(NSString*) version {
+    _version = @"1.0.0";
+    return _version;
+}
+
+@end
